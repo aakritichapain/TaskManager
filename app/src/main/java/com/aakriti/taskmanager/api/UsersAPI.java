b9176ebda9a8ac2d64925796066bb48fd57e2527
aakritichapain <akriteechapain@gmail.com>
2019-12-31 08:41:14 +0545

    UsersAPI

--- app/src/main/java/com/aakriti/taskmanager/api/UsersAPI.java
+++ app/src/main/java/com/aakriti/taskmanager/api/UsersAPI.java
@@ -1,4 +1,29 @@
 package com.aakriti.taskmanager.api;
 
+import com.aakriti.taskmanager.model.Users;
+import com.aakriti.taskmanager.serverresponse.ImageResponse;
+import com.aakriti.taskmanager.serverresponse.SignUpResponse;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
 public interface UsersAPI {
+
+    @POST ("users/signup")
+    Call<SignUpResponse> registerUser(@Body Users users);
+
+
+    @FormUrlEncoded
+    @POST("users/login")
+    Call<SignUpResponse> checkUser(@Field("username") String username, @Field("password") String password);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
 }
